8816fb677c37a18b8ec67b9a486d5815314e5890
trung <trungmoscow2002@gmail.com>
2025-06-17 07:57:25 +0700

    update

--- demo/src/main/java/bai5/service/MovieService.java
+++ demo/src/main/java/bai5/service/MovieService.java
@@ -6,12 +6,10 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
-import java.time.Duration;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
