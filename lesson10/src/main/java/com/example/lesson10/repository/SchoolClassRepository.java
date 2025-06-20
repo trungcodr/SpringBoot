2943ce1cae9223633e4f2dab2572a352e2e206fe
trung <92379274+trungcodr@users.noreply.github.com>
2025-06-21 00:05:54 +0700

    Delete lesson10 directory

--- lesson10/src/main/java/com/example/lesson10/repository/SchoolClassRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.lesson10.repository;
-
-import com.example.lesson10.entity.SchoolClass;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SchoolClassRepository extends JpaRepository<SchoolClass, Long> {
-
-}
